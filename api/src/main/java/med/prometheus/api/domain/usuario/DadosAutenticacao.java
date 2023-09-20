7f861f7b12ccc1b34d637f832fc82516fb0c9e2b
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-09-20 15:07:47 -0300

    Cofiguration SpringSecurity token and Controller de autenticação

---
+++ api/src/main/java/med/prometheus/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package med.prometheus.api.domain.usuario;
+
+public record DadosAutenticacao( String login, String senha ) {
+}
--- api/src/main/java/med/prometheus/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/prometheus/api/infra/security/SecurityConfigurations.java
@@ -2,23 +2,38 @@ package med.prometheus.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
-    @Bean
+    @Bean  // metodo para controlar a autenticação
     public SecurityFilterChain securityFilterChain( HttpSecurity http ) throws  Exception {
         // A partir da versão 3.1 do Spring Boot
         return http.csrf( csrf -> csrf.disable())
                 .sessionManagement( sm -> sm.sessionCreationPolicy( SessionCreationPolicy.STATELESS ))
                 .build();
-// Antes da versão 3.1, após isto se tornou deprcated
-//        return http.csrf().disable().sessionManagement()
+//          Antes da versão 3.1, após isto se tornou deprcated
+//              return http.csrf().disable().sessionManagement()
 //                .sessionCreationPolicy( SessionCreationPolicy.STATELESS ).and().build();
     }
+
+    @Bean  // serve para mostrar para spring com injetar o  AuthenticationManager
+    public AuthenticationManager authenticationManager ( AuthenticationConfiguration configuration ) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean // mostra o tipo de criptografia qu vai ser usado
+    public PasswordEncoder passwordEncoder() {
+        return  new BCryptPasswordEncoder();
+    }
+
 }
