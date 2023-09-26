4e572e8ab20d66204c34bb8cba456173174ae62c
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-09-26 15:47:41 -0300

    Definindo a sequencia dos filtros e autenticacao do token

--- api/src/main/java/med/prometheus/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/prometheus/api/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package med.prometheus.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,20 +12,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean  // metodo para controlar a autenticação
     public SecurityFilterChain securityFilterChain( HttpSecurity http ) throws  Exception {
         // A partir da versão 3.1 do Spring Boot
         return http.csrf( csrf -> csrf.disable())
-                .sessionManagement( sm -> sm.sessionCreationPolicy( SessionCreationPolicy.STATELESS ))
+                .sessionManagement( sm -> sm.sessionCreationPolicy( SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST,"/login")
+                .permitAll().anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-//          Antes da versão 3.1, após isto se tornou deprcated
-//              return http.csrf().disable().sessionManagement()
-//                .sessionCreationPolicy( SessionCreationPolicy.STATELESS ).and().build();
     }
 
     @Bean  // serve para mostrar para spring com injetar o  AuthenticationManager
--- api/src/main/java/med/prometheus/api/infra/security/TokenService.java
+++ api/src/main/java/med/prometheus/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.prometheus.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.prometheus.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -36,4 +37,16 @@ public class TokenService {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("prometheus.med API")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
 }
